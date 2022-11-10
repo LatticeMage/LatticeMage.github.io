9156b5a9b211a6a32da9a6f55a3652f040bbceb5
QuantumNecro <QuantumNecro@users.noreply.github.com>
2022-11-10 09:52:56 +0800

    upload

---
+++ updateKnowledge.ps1
@@ -0,0 +1,10 @@
+rmdir -R Knowledge -force
+cp -r ../Knowledge/ ./
+cp ./Converter/createTagMD.py .\Knowledge\
+cp ./Converter/moveRenameFile.py .\Knowledge\
+cp ./Converter/run.bat .\Knowledge\
+cd ./Knowledge
+rmdir .git -force
+./run.bat
+cd ..
+cp ./Knowledge.md ./Knowledge/README.md
--- upload.ps1
+++ upload.ps1
@@ -4,16 +4,8 @@ git remote set-url origin git@AAL:QuantumNecro/QuantumNecro.github.io.git
 
 git pull
 
-rmdir -R Knowledge -force
-cp -r ..\Knowledge\ ./
-cp .\createTagMD.py .\Knowledge\
-cp .\moveRenameFile.py .\Knowledge\
-cp ./run.bat .\Knowledge\
-cd .\Knowledge\
-rmdir .git -force
-./run.bat
-cd ..
-cp ./Knowledge.md ./Knowledge/README.md
+./updateKnowledge.ps1
+
 git add .
 git commit -m "upload"
 git push
