91536f4ae1d511c87117943d13e69f5e02378400
QuantumNecro <QuantumNecro@users.noreply.github.com>
2022-11-12 13:13:00 +0800

    upload

--- Knowledge.ps1
+++ Knowledge.ps1
@@ -1,8 +1,7 @@
 rmdir -R Knowledge -force
 
-Copy-Item -Path (Get-Item -Path "../Knowledge/" -Exclude ('*/git/*', '*/vs/*', '*/obsidian/*')).FullName -Destination ./Knowledge -Recurse -Force
+Copy-Item -Path (Get-Item -Path "../Knowledge/" -Exclude ('*.git/*', '*.vs/*', '*.obsidian/*')).FullName -Destination ./Knowledge -Recurse -Force
 
-cp -r  ./
 cp ./Converter/createTagMD.py .\Knowledge\
 cp ./Converter/moveRenameFile.py .\Knowledge\
 cp ./Converter/checkSubfolder.py .\Knowledge\
