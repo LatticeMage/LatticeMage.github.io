3787e44f3da7943309fa8a4cc58076929907c6b0
QuantumNecro <QuantumNecro@users.noreply.github.com>
2022-11-12 13:25:30 +0800

    upload

--- Knowledge.ps1
+++ Knowledge.ps1
@@ -3,10 +3,7 @@ rmdir -R Knowledge -force
 New-Item -ItemType Directory -Force -Path ./Knowledge # directory must exist
 Copy-Item ../Knowledge/* ./Knowledge -Exclude @(".git",".vs",".obsidian") -Recurse
 
-cp ./Converter/createTagMD.py .\Knowledge\
-cp ./Converter/moveRenameFile.py .\Knowledge\
-cp ./Converter/checkSubfolder.py .\Knowledge\
-cp ./Converter/run.bat .\Knowledge\
+cp ./Converter/* .\Knowledge\
 cd ./Knowledge
 
 ./run.bat
