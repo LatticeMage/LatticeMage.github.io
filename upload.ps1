91a4151dfbfe66666ff2181a2f4b2547202b291a
LatticeMage <LatticeMage@users.noreply.github.com>
2023-03-10 19:00:49 +0800

    upload

--- upload.ps1
+++ upload.ps1
@@ -1,6 +1,6 @@
 git config --local user.name "LatticeMage"
 git config --local user.email "LatticeMage@users.noreply.github.com"
-git remote set-url origin git@AAL:LatticeMage/LatticeMage.github.io.git
+git remote set-url origin git@LM:LatticeMage/LatticeMage.github.io.git
 
 git pull
 
