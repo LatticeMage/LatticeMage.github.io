381f859687bbebd9b45a8cf9027c1f3e3c7e4988
LatticeMage <LatticeMage@users.noreply.github.com>
2023-07-28 00:17:35 +0800

    upload

---
+++ _includes/uploadXLSX.js
@@ -0,0 +1,15 @@
+function uploadXLSX(elementId) {
+    const xlsxFile = document.getElementById(elementId).files[0];
+    if (!xlsxFile) {
+        alert('No file selected!');
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, {type: 'array'});
+        return workbook;
+    };
+    reader.readAsArrayBuffer(xlsxFile);
+}
