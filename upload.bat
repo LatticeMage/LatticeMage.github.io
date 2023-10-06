695dbb8e593a47e4345db987f1faefb28cd9d160
LatticeMage <LatticeMage@users.noreply.github.com>
2023-10-06 21:28:47 +0800

    upload

--- upload.bat
+++ upload.bat
@@ -2,8 +2,9 @@ git config --local user.name "LatticeMage"
 git config --local user.email "LatticeMage@users.noreply.github.com"
 git remote set-url origin git@LM:LatticeMage/LatticeMage.github.io.git
 
-git pull
+git submodule update --recursive --remote
 
+git pull
 git add .
 git commit -m "upload"
 git push
